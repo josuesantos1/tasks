725a068e200bd1764dda5e74847d1c283bc9b29a
josue santos <josue.santos.dev@gmail.com>
2025-01-27 18:21:37 -0300

    added datomic config

---
+++ src/tasks/database/config.clj
@@ -0,0 +1,27 @@
+(ns tasks.database.config
+  (:require [datomic.api :as d]))
+
+(def uri "datomic:mem://tasks")
+(d/create-database uri)
+(def datomic (d/connect uri))
+
+(def schema
+  [{:db/ident       :task/id
+    :db/valueType   :db.type/uuid
+    :db/cardinality :db.cardinality/one
+    :db/unique      :db.unique/identity}
+   {:db/ident       :task/title
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident       :task/description
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident       :task/status
+    :db/valueType   :db.type/keyword
+    :db/cardinality :db.cardinality/one}
+   {:db/ident :task.status/todo}
+   {:db/ident :task.status/closed}
+   {:db/ident :task.status/doing}
+   {:db/ident :task.status/done}])
+
+(defn create-schema [] (d/transact datomic schema))
