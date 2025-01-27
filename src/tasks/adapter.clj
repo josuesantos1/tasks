05c30f45ab4b85a78aa2fdd2cdabffa5abd5045d
josue santos <josue.santos.dev@gmail.com>
2025-01-27 18:52:17 -0300

    added adapter to schema->out and schemas->out

--- src/tasks/adapter.clj
+++ src/tasks/adapter.clj
@@ -1,12 +1,24 @@
 (ns tasks.adapter 
   (:require
-    [schema.core :as s]
+   [schema.core :as s]
    [tasks.schema.model :as schema.model]
-   [tasks.wire.in :as wire.in]))
+   [tasks.schema.in :as schema.in]
+   [tasks.schema.out :as schema.out]))
 
 (s/defn in->schema :- schema.model/Task
-  [{:keys [title description]} :- wire.in/Task]
+  [{:keys [title description]} :- schema.in/Task]
   {:id          (random-uuid)
    :title       title 
    :description description
    :status      :task/todo})
+
+(s/defn schema->out :- schema.out/Task
+  [{:task/keys [id title description status]} :- schema.model/Task]
+  {:id          id
+   :title       title
+   :description description
+   :status      (name status)})
+
+(s/defn schemas->out :- schema.out/Tasks
+  [tasks :- schema.model/Tasks]
+  (map schema->out tasks))
