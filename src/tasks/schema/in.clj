e00384d7429c4f3c8ac321ca07cd17114e0196b6
josue santos <josue.santos.dev@gmail.com>
2025-01-27 21:41:03 -0300

    added schema in to update task

--- src/tasks/schema/in.clj
+++ src/tasks/schema/in.clj
@@ -2,6 +2,14 @@
   (:require
    [schema.core :as s]))
 
+(s/def Status
+  (s/enum "todo" "closed" "doing" "done"))
+
 (s/defschema Task 
   {:title s/Str
    (s/optional-key :description) s/Str})
+
+(s/defschema UpdateTask
+  {(s/optional-key :title)       s/Str
+   (s/optional-key :description) s/Str
+   (s/optional-key :status)      Status})
